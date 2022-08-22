bc4c93c07ffce78c9f022eb731fe2f34b6d49613
katampoi <simonkatampoi@student.moringaschool.com>
2022-08-22 23:07:22 +0300

    Complete Assignment

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,8 +6,21 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+  class PartnerError < StandardError
+    def message
+      "you should get married!"
+    end
   end
 
 end
